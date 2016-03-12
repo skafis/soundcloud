b55623e892ce816c3e4d1095971f7e2732fd3963
skafis <franciskipchumba5@gmail.com>
2016-03-12 14:46:30 +0300

    add an interface class

--- app/src/main/java/com/example/entsfrank/soundcloud/Config.java
+++ app/src/main/java/com/example/entsfrank/soundcloud/Config.java
@@ -4,8 +4,8 @@ package com.example.entsfrank.soundcloud;
  * Created by frank on 3/12/16.
  */
 public class Config {
-    public static final String Client_ID = "90e97eeee996fb1d5e6551ced37a1a6d";
-    public static final String Client_ID = "https://api.soundcloud.com";
+    public static final String String CLIENT_ID = "90e97eeee996fb1d5e6551ced37a1a6d";
+    public static final String String API_URL = "https://api.soundcloud.com";
 
 
 }
--- app/src/main/java/com/example/entsfrank/soundcloud/Track.java
+++ app/src/main/java/com/example/entsfrank/soundcloud/Track.java
@@ -1,7 +1,38 @@
 package com.example.entsfrank.soundcloud;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by frank on 3/12/16.
  */
 public class Track {
+    @SerializedName("title")
+    private String mTitle;
+
+    @SerializedName("id")
+    private int mID;
+
+    @SerializedName("stream_url")
+    private String mStreamURL;
+
+    @SerializedName("artwork_url")
+    private String mArtworkURL;
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public int getID() {
+        return mID;
+    }
+
+    public String getStreamURL() {
+        return mStreamURL;
+    }
+
+    public String getArtworkURL() {
+        return mArtworkURL;
+    }
+
 }
+
