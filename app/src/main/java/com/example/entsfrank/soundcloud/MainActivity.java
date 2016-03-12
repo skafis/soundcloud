0dc5b61b270f8f8e87a508b908e8384a6aa99975
skafis <franciskipchumba5@gmail.com>
2016-03-12 15:21:13 +0300

    configure the Scservice class to to have an adapter

--- app/src/main/java/com/example/entsfrank/soundcloud/MainActivity.java
+++ app/src/main/java/com/example/entsfrank/soundcloud/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.entsfrank.soundcloud;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint(Config.API_URL).build();
+        SCService scService = restAdapter.create(SCService.class);
+        scService.getRecentTracks(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date()), new Callback<List<Track>>() {
+            @Override
+            public void success(List<Track> tracks, Response response) {
+                Log.d(TAG, "First track title: " + tracks.get(0).getTitle());
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.d(TAG, "Error: " + error);
+            }
+        });
     }
 }
