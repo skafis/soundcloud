c4fff30be670a4b7b20848428d127f0583c4a81d
skafis <franciskipchumba5@gmail.com>
2016-03-12 15:21:44 +0300

    add SCservice class

--- app/src/main/java/com/example/entsfrank/soundcloud/SCService.java
+++ app/src/main/java/com/example/entsfrank/soundcloud/SCService.java
@@ -1,7 +1,18 @@
 package com.example.entsfrank.soundcloud;
 
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Query;
+
 /**
  * Created by frank on 3/12/16.
  */
-public class SCService {
+
+public interface SCService {
+
+    @GET("/tracks?client_id=" + Config.CLIENT_ID)
+    public void getRecentTracks(@Query("created_at[from]") String date, Callback<List<Track>> cb);
+
 }
